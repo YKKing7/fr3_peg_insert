be235c938332ad3621bb2c80bcd5129240e01484
me <2691047852@qq.com>
2026-06-28 18:55:11 +0800

    Initial commit

---
+++ source/fr3_peg_insert/fr3_peg_insert/tasks/direct/fr3_peg_insert/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Fr3-Peg-Insert-Direct-v0",
+    entry_point=f"{__name__}.fr3_peg_insert_env:Fr3PegInsertEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.fr3_peg_insert_env_cfg:Fr3PegInsertEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+    },
+)
